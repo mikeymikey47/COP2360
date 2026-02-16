9a2d2e92e423712bfeee521ab43d019f8e9e4850
mikeymikey47 <scholaredumecow@gmail.com>
2026-02-15 22:22:30 -0500

    Add files via upload
    attempt 1 to upload VSC program file with code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{ 
+    //Opening the fridge door
+    static void Main()
+    {
+        Wine w = new Wine(19.99m); // we used Wine in class so imma use it here
+        Console.WriteLine($"Price: {w.Price}"); //this is the price you gonna pay for your yummylicious adult grape juice
+    }
+
+}   // closing frige door and aligning the curly braces
+
+public class Wine
+{
+    public decimal Price; //public come on in, the water's fine
+    private int superPrivateSecret = 420; // **music** cant touch this
+
+    public Wine(decimal price)
+    {
+        Price = price;
+    }
+}
