76fe5ed3d054f68d7be6bffbdb856c4f6c68917f
mikeymikey47 <scholaredumecow@gmail.com>
2026-03-01 23:07:09 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,23 @@
 ﻿using System;
-namespace DivisionConversionExceptions
-{
-    class Program
+
+class Program
+{ 
+    //Opening the fridge door
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.Write("I must inquire thee. Shall thyself provide a first number?"); // number numero uno acquired
-            string input1 = Console.ReadLine();
+        Wine w = new Wine(19.99m); // we used Wine in class so imma use it here
+        Console.WriteLine($"Price: {w.Price}"); //this is the price you gonna pay for your yummylicious adult grape juice
+    }
 
-            Console.Write("I summon thee once more. Produce a second number, if thou may."); // number numero dos acquired
-            string input2 = Console.ReadLine();
+}   // closing frige door and aligning the curly braces
 
-            try // here we go. scumbag detention protocol activated
-            {
-                int result = Divide(input1, input2);
-                Console.WriteLine($"Result: {result}");
-            }
-            catch (FormatException) // uh oh, this number aint a number. uh uh, nope, not happening
-            {
-                Console.WriteLine("I must stop thee. Thy numbers mayhaps contain...corruption..");
-            }
-            catch (DivideByZeroException) // "911 whats ur emergency?" hi um this user is trying to divide by zero
-            {
-                Console.WriteLine("Dost thou believeth 0 a divisor? Thou art...wrong.");
-            }
-            catch (OverflowException) // the console is sickened by the user's greed. too many numbers!!!
-            {
-                Console.WriteLine("Thy number art as collosal as thy incompetence.");
-            }
-            catch (Exception ex) // what the hell happened here? w-what....wha? oh nah
-            {
-                Console.WriteLine($"Thou hast committed grave errors of calibers...unimagineable.");
-                Console.WriteLine($"Thy punishment is...death. ");
-            }
-        }
-        static int Divide(string a, string b)
-        {
-            int num1 = Convert.ToInt32(a);
-            int num2 = Convert.ToInt32(b);
-            return num1 / num2;
-        }
+public class Wine
+{
+    public decimal Price; //public come on in, the water's fine
+    private int superPrivateSecret = 420; // **music** cant touch this
+
+    public Wine(decimal price)
+    {
+        Price = price;
     }
 }
-            
-
